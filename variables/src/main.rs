964f5177814f810ce525652b90e31c8bd2d39642
jtmedalla <243247066+jtmedalla@users.noreply.github.com>
2026-08-27 14:27:38 +0800

    docs: add comments in variables tutorial file

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,15 @@
+// const variables can be declared in global scope
 const PI: f64 = 3.1415;
 
 fn main() {
+    // the "mut" keyword needs to be added to allow for mutability for a variable
     let mut number = 10;
     println!("number = {number}");
 
     number += 1;
     println!("number = {number}");
 
+    // const variables are usually named in snake case with all letters in UPPERCLASS
     const ONE_MINUTE: i32 = 60;
     const ONE_HOUR: i32 = ONE_MINUTE * 60;
 
