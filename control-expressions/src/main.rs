f8777bb45ab672d672aafc00536455cc96282967
jtmedalla <243247066+jtmedalla@users.noreply.github.com>
2026-08-30 11:34:31 +0800

    feat: add more functions in tutorial

--- control-expressions/src/main.rs
+++ control-expressions/src/main.rs
@@ -7,6 +7,18 @@ fn main() {
     } else {
         println!("Password is too short...");
     }
+
+    dbg!(get_response("Hello, Bob!"));
+    dbg!(get_response("How are you?"));
+    dbg!(get_response("Good"));
+    dbg!(get_response("Is this a cat in a hat?"));
+
+    analyse_number(100);
+
+    let n = 10;
+    // values to return must be of same type
+    let odd_even = if n % 2 == 0 {"Even"} else {"Odd"};
+    dbg!(odd_even);
 }
 
 fn check_length(password: &str) {
@@ -20,7 +32,7 @@ fn check_length(password: &str) {
 }
 
 fn long_enough(password: &str) -> bool {
-    let length = password.len();
+    let length = password.chars().count();
 
     if length >= 10 {
         return true;
@@ -30,4 +42,29 @@ fn long_enough(password: &str) -> bool {
 
     // can also be: 
     // return length >= 10;
+}
+
+fn get_response(input: &str) -> &str {
+    let lowered: String = input.to_lowercase();
+
+    // order matters. first evaluated is the only one that counts
+    if lowered.contains("hello") {
+        return "Hello there!";
+    } else if lowered.contains("how are you") {
+        return "Good, and you";
+    } else if lowered.contains("good") {
+        return "Good is good.";
+    } else {
+        return "I don't understand";
+    }
+}
+
+fn analyse_number(n: i32) {
+    if n > 0 {
+        println!("{n} is greater than 0!");
+    } else if n > 10 {
+        println!("{n} is greater than 10!");
+    } else {
+        println!("{n} is a cool number!");
+    }
 }
