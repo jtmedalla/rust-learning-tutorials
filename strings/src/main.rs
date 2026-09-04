2353ea05f474907b3e0e5a9f0083da9123a941bf
jtmedalla <243247066+jtmedalla@users.noreply.github.com>
2026-09-04 11:17:00 +0800

    feat: add contnet to string slices tutorial

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -3,8 +3,42 @@ fn main() {
     let first_word = get_first_word(&sentence);
     println!("first_word={:?}", first_word);
     sentence.clear();
+
+    let sentence2 = String::from("Bob loves Chinese food.");
+
+    /*
+        use slice notation to get a reference to a portion of a string
+        slice data structure stores start and end of
+    */ 
+    let name = &sentence2[0..3];
+    let food = &sentence2[10..17];
+    dbg!(name, food);
+
+    // s1 and s2 are the same
+    let s1 = &sentence2[..3];
+    let s2 = &sentence2[0..3];
+
+    // s3 is sliced from start to end of the string
+    let s3 = &sentence2[10..];
+
+    // slice entire string
+    let s4 = &sentence[..];
+
+    // slice must end on a valid byte. consider multi byte characters
+    let name = String::from("Björn");
+    dbg!(&name[..4]);
+
+    let mut sentence = String::from("Holy bananas!");
+    let word = get_first_word2(&sentence);
+    dbg!(word);
+
+    // string literals as slices. Slice pointing to specific binary
+    let name = "Bob";
+    dbg!(name);
 }
 
+
+// manual slicing
 fn get_first_word(sentence: &String) -> usize {
     let bytes = sentence.as_bytes();
 
@@ -15,4 +49,17 @@ fn get_first_word(sentence: &String) -> usize {
     }
 
     return sentence.len();
+}
+
+// using string slices 
+fn get_first_word2(sentence: &String) -> &str {
+    let bytes = sentence.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &sentence[..i];
+        }
+    }
+
+    return &sentence[..];
 }
